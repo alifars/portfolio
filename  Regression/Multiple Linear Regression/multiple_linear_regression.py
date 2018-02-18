2a974839650b583c44b8bbe0e1b1fdcd14e983e0
alifars <ali.farsi.nejad@gmail.com>
2018-02-17 22:27:28 -0500

    added iWordSense

---
+++  Regression/Multiple Linear Regression/multiple_linear_regression.py
@@ -0,0 +1,27 @@
+# Multiple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+# Avoiding the Dummy Variable Trap
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+
+
+
+
+# Fitting Multiple Linear Regression to the Training set
+
+
+# Predicting the Test set results
+
